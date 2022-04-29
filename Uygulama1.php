cd0428a10c4786eed34e1a289809c6259a643e53
dyzzta <87264688+dyzzta@users.noreply.github.com>
2022-04-29 16:01:32 +0300

    Add files via upload
    for search someword in the text

---
+++ Uygulama1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uygulama1</title>
+</head>
+<body>
+
+
+Aradiginiz sey : <?php echo $_POST["ara"]; ?><br>
+Metin : <br>
+<?php echo $_POST["metin"]; ?><br>
+Sonuc : <?php echo strpos( $_POST["metin"], $_POST["ara"]);?>
+
+<?php
+$pos = strpos( $_POST["metin"], $_POST["ara"]);
+if ($pos < 3) {
+    echo ("Tekrar deneyin");
+} else {
+    echo ("Sonuc :");
+    echo strpos( $_POST["metin"], $_POST["ara"]);;
+}
+?>
+
+</body>
+</html>
